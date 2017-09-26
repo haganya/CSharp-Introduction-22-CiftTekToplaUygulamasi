10f29e2121173aa4fb59859398c7da521ec62512
Hakan Akgül <hakanakgul34@gmail.com>
2017-09-26 20:24:25 +0300

    created at 09.08.2017 (dd.mm.yyyy)

---
+++ 20170809-5Con_CiftTekToplaUygulamasi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20170809_5Con_CiftTekToplaUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcının girdiği 20 sayıdan çiftleri ve tekleri toplayıp ekrana yazan uygulamayı yazınız.
+            //Uygulama içerisinde foreach döngüsü kullanılmalıdır.
+
+            int[] sayilar = new int[20];
+            int tekToplam = 0, ciftToplam = 0;
+
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.Write((i+1)+". Sayıyı giriniz : ");
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int item in sayilar)
+            {
+                if (item % 2 == 0)
+                {
+                    ciftToplam += item;
+                }
+                else
+                {
+                    tekToplam += item;
+                }
+            }
+
+            Console.WriteLine("Tek sayıların toplamı : {0}",tekToplam);
+            Console.WriteLine("Çift sayıların toplamı : {0}", ciftToplam);
+
+            //eğer metinleri tek bir Console.Write(); içerisinde yazmak istersek ve bunların yine de alt alta yazılmasını istiyorsak \n kullanırız \ karakteri kaçış karakteridir. Aynı zamanda \"  kullanarak string içerisinde çift tırnak işareti de kullanabiliriz.
+            // \t 8 karakterlik boşluk bırakır TAB tuşuna basmış gibi olur.
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ 20170809-5Con_CiftTekToplaUygulamasi/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("20170809-5Con_CiftTekToplaUygulamasi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("20170809-5Con_CiftTekToplaUygulamasi")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("739e38d0-d9de-4aca-9727-7c95cfb20237")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
